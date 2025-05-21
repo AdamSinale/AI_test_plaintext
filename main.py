4535cd08e1588b07996f66e5b1e941d52662e744
AdamSinale <adam.sinale@gmail.com>
2025-05-21 18:39:01 +0300

    ending to another file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 from api_called import send_api_key
+from random_called import send_other_str
 
 def main():
     # Hardcoded API key (should NEVER be committed to git)
     api_key = "abstract 4f8aeb7c9123d4f5a6b7c8d9e0f1a2b3"
+    safeStr = "safe string"
     send_api_key(api_key)
+    send_other_str(safeStr)
 
 if __name__ == "__main__":
     main()
---
+++ random_called.py
@@ -0,0 +1,3 @@
+
+def send_other_str(string: str):
+    print("This file receives API key")
